426f551fc8b7596ac8922cd0d44de9eda3805176
Shiva <dasshivashish11@gmail.com>
2022-11-22 03:36:00 +0530

    Basic build of jvm capable of running test file

--- jvm/src/main.rs
+++ jvm/src/main.rs
@@ -4,15 +4,16 @@ extern crate javaclass;
 extern crate rustop;
 extern crate log;
 mod logger;
+mod frame;
 
 use javaclass::read_classfile;
 use rustop::opts;
 use log::LevelFilter;
-use std::process::exit;
+use frame::Frame;
 use log::*;
 
 fn main() {
-    let (args, rest) = opts! {
+    let (args, _) = opts! {
         synopsis "A Java virtual machine implementation.";
         version "0.0.1";
         opt verbose:bool, desc:"Be verbose.";
@@ -43,6 +44,15 @@ fn main() {
        exit!("This JVM only supports class files upto version 52 (Java 1.8) but the given file has version {} (Java 1.{})", class.major_version, class.major_version - 44);
    }
 
+   debug!("Running class file version: {} (Java 1.{})", class.major_version, class.major_version - 44);
+   let main = class.find_method("main", "([Ljava/lang/String;)V");
+   debug!("Found method main(String[] args)");
+   debug!("Starting main");
+   
+   let mut mframe = Frame::new(&main, &class.constant_pool);
+   mframe.exec();
+   debug!("Main method ended");
+   debug!("Shutting down VM. Goodbye!");
 }
 
 
