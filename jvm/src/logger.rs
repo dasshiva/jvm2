65b8803e5be73e8bf21776cb80d98f1c3bada0f6
Shiva <dasshivashish11@gmail.com>
2022-11-23 09:54:59 +0530

    New initial commit

---
+++ jvm/src/logger.rs
@@ -0,0 +1,39 @@
+use crate::log::{Level, Record, Metadata, SetLoggerError, LevelFilter};
+use std::process;
+
+extern crate proc_macro;
+struct Logger;
+
+ #[macro_export]
+macro_rules! exit {
+    ($($arg:tt)+) => { 
+        { 
+            ::log::error!("{}", format!($($arg)+));
+            std::process::exit(1)
+        }
+    }
+}
+
+
+static LOGGER: Logger = Logger;
+impl log::Log for Logger {
+    fn enabled(&self, met: &Metadata) -> bool {
+        #[cfg(debug_assertions)]
+        return met.level() <= Level::Trace;
+
+        #[cfg(not(debug_assertions))]
+        return met.level() <= Level::Error;
+    }
+
+    fn log(&self, rec: &Record) {
+        if self.enabled(rec.metadata()) {
+            eprintln!("[{}] {}:{} {}", rec.level(), rec.file().unwrap(), rec.line().unwrap(), rec.args());
+        }
+    }
+
+    fn flush(&self) {}
+}
+
+pub fn init(lf: LevelFilter) -> Result<(), SetLoggerError> {
+    log::set_logger(&LOGGER).map(|()| log::set_max_level(lf))
+}
